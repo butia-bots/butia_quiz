d3a294c6e95ddf950861d3e492aca735d1161e9f
jardeldyonisio <jardel.dyonisio@hotmail.com>
2022-10-17 16:39:52 -0300

    Add new resources - LARC 2022

--- nodes/butia_quiz_node.py
+++ nodes/butia_quiz_node.py
@@ -11,7 +11,7 @@ from nltk.corpus import stopwords
 from Levenshtein import distance
     
 PACKAGE_DIR = rospkg.RosPack().get_path("butia_quiz")
-DORIS_PERSONAL_QUESTIONS_FILEPATH = os.path.join(PACKAGE_DIR, "resources/doris_personal_questions.json")
+DORIS_PERSONAL_QUESTIONS_FILEPATH = os.path.join(PACKAGE_DIR, "resources/larc_2022_questions.json")
 
 def merge_question_array(question_array):
     question = ''
