b3c0c436cd816ab5cd01e05c9abf41110148f198
crislmfroes <cris.lima.froes@gmail.com>
2023-10-02 10:42:55 -0300

    replace gpt-3.5-turbo by llama 2 from replicate

--- nodes/butia_quiz_node.py
+++ nodes/butia_quiz_node.py
@@ -14,6 +14,10 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import Replicate
+from langchain.embeddings import HuggingFaceHubEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
     
 PACKAGE_DIR = rospkg.RosPack().get_path("butia_quiz")
 DORIS_PERSONAL_QUESTIONS_FILEPATH = os.path.join(PACKAGE_DIR, "resources/where_is_this.json")
@@ -90,10 +94,14 @@ if __name__ == "__main__":
     rospy.init_node("butia_quiz_node", anonymous=False)
     pkg_path = rospkg.RosPack().get_path('butia_quiz')
     pdf_loader = PyPDFLoader(file_path=os.path.join(pkg_path, "resources", "docs", "Rulebook.pdf"))
-    knowledge_index = VectorstoreIndexCreator().from_loaders([pdf_loader,])
-
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    question_answering_chain = RetrievalQA.from_chain_type(llm=llm, retriever=knowledge_index.vectorstore.as_retriever())
+    documents = pdf_loader.load()
+    embeddings = HuggingFaceHubEmbeddings()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    docsearch = Chroma.from_documents(texts, embeddings)
+    #llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = Replicate(model="meta/llama-2-70b:a52e56fee2269a78c9279800ec88898cecb6c8f1df22a6483132bea266648f00", model_kwargs={"temperature": 0.0, "max_length": 500, "top_p": 1})
+    question_answering_chain = RetrievalQA.from_chain_type(llm=llm, retriever=docsearch.as_retriever())
     butia_quiz_service_param = rospy.get_param("servers/butia_quiz/service")
     rospy.Service(butia_quiz_service_param, ButiaQuizComm, answer_question)
 
